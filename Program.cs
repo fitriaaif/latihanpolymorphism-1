6583abbe1304786decaafaf155dc03a606ed713d
Fitria Amanda <101631102+fitriaaif@users.noreply.github.com>
2022-06-19 18:30:17 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Cat cat = new Cat();
+            animal = cat;
+            //cat.Speak();
+            animal.Speak();
+
+            Duck duck = new Duck();
+            animal = duck;
+            //duck.Speak();
+            animal.Speak();
+
+            Tiger tiger = new Tiger();
+            animal = tiger;
+            //tiger.Speak();
+            animal.Speak();
+
+            Console.ReadLine();
+        }
+    }
+}
